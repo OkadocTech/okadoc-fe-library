ff1fc5e526741a91d9f22f25a1ce7195f8b88e99
ben <jordanbensiahaan@gmail.com>
2021-09-01 19:41:16 +0700

    updates packages

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: ['./src/index.js'],
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'libs.js',
+        filename: 'index.js',
         library: 'okalibs',
         libraryTarget: 'commonjs2'
     },
