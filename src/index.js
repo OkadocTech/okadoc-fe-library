2bbe98c15ac24458f212f43d6bb00ce6556c1627
ben <jordanbensiahaan@gmail.com>
2021-09-01 23:10:59 +0700

    finally

--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-export { sum } from '@libs/math';
-export { multiply } from '@libs/math';
-export { default as math } from '@libs/math';
+export { default as OkaHMAC } from '@libs/kong-hmac';
--- src/libraries/math.js
+++
@@ -1,7 +0,0 @@
-export const sum = (x, y) => x + y;
-
-export const multiply = (x, y) => x * y;
-
-const defaultFunction = () => console.log('Hello from Okadoc [OKA LIBS]');
-
-export default defaultFunction;
